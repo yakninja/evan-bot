d3102614b4fc819b5cfd6c110752807a26178c42
yakninja <andrey@yakninja.pro>
2020-01-23 14:39:52 +0100

    work work

--- application.py
+++ application.py
@@ -1,26 +1,34 @@
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
-import logging
-from config import TOKEN
+from flask import Flask
 
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-                    level=logging.INFO)
+# print a nice greeting.
+def say_hello(username = "World"):
+    return '<p>Hello %s!</p>\n' % username
 
+# some bits of text for the page.
+header_text = '''
+    <html>\n<head> <title>EB Flask Test</title> </head>\n<body>'''
+instructions = '''
+    <p><em>Hint</em>: This is a RESTful web service! Append a username
+    to the URL (for example: <code>/Thelonious</code>) to say hello to
+    someone specific.</p>\n'''
+home_link = '<p><a href="/">Back</a></p>\n'
+footer_text = '</body>\n</html>'
 
-def start(update, context):
-    context.bot.send_message(chat_id=update.effective_chat.id, text="Привет :)")
+# EB looks for an 'application' callable by default.
+application = Flask(__name__)
 
+# add a rule for the index page.
+application.add_url_rule('/', 'index', (lambda: header_text +
+    say_hello() + instructions + footer_text))
 
-def echo(update, context):
-    context.bot.send_message(chat_id=update.effective_chat.id, text=update.message.text)
+# add a rule when the page is accessed with a name appended to the site
+# URL.
+application.add_url_rule('/<username>', 'hello', (lambda username:
+    header_text + say_hello(username) + home_link + footer_text))
 
-
-updater = Updater(token=TOKEN, use_context=True)
-dispatcher = updater.dispatcher
-
-start_handler = CommandHandler('start', start)
-dispatcher.add_handler(start_handler)
-
-echo_handler = MessageHandler(Filters.text, echo)
-dispatcher.add_handler(echo_handler)
-
-updater.start_polling()
+# run the app.
+if __name__ == "__main__":
+    # Setting debug to True enables debug output. This line should be
+    # removed before deploying a production app.
+    application.debug = True
+    application.run()
